68310e9b1f86152a794ed932feb6f86c19888cd8
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-11-22 08:32:13 +0100

    Create resistorCombosWIP.js

---
+++ wip_ideas/resistorCombosWIP.js
@@ -0,0 +1,97 @@
+var Resistor = function(resistorValue){
+  if( ! isNaN(resistorValue) ){
+    console.log("decimal !");
+    // maybe the number is within a string, let's make it ready for later / standardize that !
+    if( typeof resistorValue === "string" ) this.resValue = Number(resistorValue);
+    else this.resValue = resistorValue; 
+    //this.resStr = resistorValue.toString(); // we could also have added "Ω" at the end
+    this.resStr = resistorValue + 'Ω'; // alternative: we add "Ω" at the end ;)
+  }
+  else {
+    // get string stuff until first char: if it's Ohm, strip it, else if maj/min supported char, deduce value
+    console.log("string !");
+    // do what the above comment say, & finally just use only the unit char OR add to it the "Ω" at the end
+    // get the unity part
+    var unity = resistorValue.substr( getUnityIdx(resistorValue) );
+    // clean it up
+    if (unity.substr(-1) === "Ω" ) unity = unity.substr(0, unity.length-1);
+    // get actual value
+    //if( unity !== '') this.resValue = applyExp( Number( resistorValue.substr(0, getUnityIdx(resistorValue) ) ), unity);
+    if( unity !== '') this.resValue = applyExp2( Number( resistorValue.substr(0, getUnityIdx(resistorValue) ) ), unity);
+    else this.resValue = Number( resistorValue.substr(0, getUnityIdx(resistorValue) ) ); 
+    //this.resStr = resistorValue;
+    this.resStr = resistorValue + 'Ω'; // alternative: we add "Ω" at the end ;)
+  }
+  // in case we don't wanna use the 'var x = new Resistor(..)' syntax, but instead 'var x = Resistor(..)'
+  return { resValue: this.resValue, resStr: this.resStr};
+}
+
+// get units & value from resistor values passed as trings ( may or not contain the ending "Ohm" symbol and/or n,µ,.. )
+getUnityIdx = function(valueStr){
+  var valueStrArr = valueStr.split('');
+  for( var i=0; i<= valueStrArr.length; i++){
+    if (isNaN(parseInt(valueStrArr[i]))) {
+      idx = i;
+      return idx;
+      break;
+    }
+  }
+}
+
+var exps2 = [
+  ['n', 1e-9 ],
+  ['µ', 1e-6 ],
+  ['m', 1e-3 ],
+  ['k',  1e3 ],
+  ['M',  1e6 ],
+  ['G',  1e9 ],
+  ['T', 1e12 ],
+];
+
+applyExp2 = function(theValue, theUnity){
+  if( theUnity !== ''){
+    // loop over the exps array & try to see if unity matches one of the array's[0] items
+    // if so, update the resValue using the corresponding array's[1] item
+    for( var i=0; i<= exps2.length; i++){
+      if (exps2[i][0] === theUnity) {
+        return theValue * exps2[i][1]; 
+        break;
+      }
+    }
+  }
+  return theValue;
+}
+
+resistors = [
+  /* Ω - Ohm */
+  47,
+  68,
+  150,
+  200,
+  270,
+  330,
+  470,
+  510,
+  560,
+  /* kΩ - KiloOhm */
+  '1k',
+  '1.5k',
+  '2.2k',
+  '3.9k',
+  '4.7k',
+  '10k',
+  '22k',
+  '27k',
+  '36k',
+  '47k',
+  '100k',
+  '200k',
+  '330k',
+  /* MΩ - MegaOhm */
+  '1M',
+  '20M',
+  '1100M',
+];
+
+resistorsStack = [];
+
